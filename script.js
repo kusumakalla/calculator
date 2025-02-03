fd363f9ceaeb5a42acceb33c49882283c018ca55
Kusuma <Kusumakalla12@gmail.com>
2025-02-03 11:46:58 +0530

    Add functions for each operator
    create functions for add, substract, multiply and divide and create operate function to select operator

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(7, 2));
+console.log(multiply(4, 2));
+
+function operate(operator, operand1, operand2) {
+}
