9139736b6907d019909b32f652d22ed582bdd0ae
kusuma <kusumakalla12@gmail.com>
2025-02-07 15:51:58 +0530

    Add function to limit input and optput
    The limitResult funtion limits the number to 9 digits and checking
    length limits the inputs to 9

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    console.log(`${a} *${b}`);
+    console.log(`${a} /${b}`);
     return a / b;
 }
 
@@ -28,6 +28,31 @@ let operand1 = "0";
 let operand2 = "0";
 let result = "";
 
+//function to limit the digits in display for decimal values
+const displayLength = 9;
+function limitResult(result){
+    let totalLenth = result.toString().length;
+    let decimalValue = result.toString().indexOf(".");
+    console.log(decimalValue);
+    if(decimalValue < 0){
+        if(totalLenth < 10) return result;
+        else {
+            return Number(result.toString().slice(0,9)); 
+        }
+    }
+    else{
+        nonDecimalLength = decimalValue;
+        decimalLength = totalLenth - (nonDecimalLength +1);
+        roundOffValue = displayLength - (nonDecimalLength + 1);
+        if(roundOffValue < 0){
+            return Number(result.toString().slice(0,9));
+        }
+        else
+            return Number(Number(result).toFixed(roundOffValue));
+        
+    }
+}
+//function to select operation based on operator
 function operate(operator, operand1, operand2) {
     switch(operator){
         case "+" : return add(operand1, operand2)
@@ -45,6 +70,9 @@ display.textContent = "0";
 
 let numArray = [...numButtons];
 display.innerHTML = '';
+// to limit length of inputs
+let length1 = 0;
+let length2 = 0;
 
 numArray.forEach(btn => {
     btn.addEventListener('click', (e) => {
@@ -52,14 +80,21 @@ numArray.forEach(btn => {
             display.innerHTML = e.target.value;
             operand1 = e.target.value;
             console.log("start");
+            length1 = 1;
         }
         else if(operator == ""){
-            display.innerHTML += e.target.value;
+            if(length1 > 8){
+                display.innerHTML = operand1;
+            }
+            else{
+            display.innerHTML += (e.target.value);
+            length1 += 1;
             operand1 += e.target.value;
             console.log("middle")
+            }
         }
         else if(result == ""){
-            display.innerHTML += e.target.value;
+            display.innerHTML += (e.target.value);
             operand2 += e.target.value;
             console.log("second")
         }
@@ -67,11 +102,18 @@ numArray.forEach(btn => {
             display.innerHTML = e.target.value;
             operand2 = e.target.value;
             console.log("second start with opr")
+            length2 = 1;
         }
         else {
-            display.innerHTML += e.target.value;
+            if(length2 >0){
+                display.innerHTML = operand2;
+            }
+            else{
+            display.innerHTML += (e.target.value);
+            length2 += 1;
             operand2 += e.target.value;
             console.log("second continue with opr")
+            }
         }
     })
 });
@@ -83,15 +125,16 @@ let operatorBtn = document.querySelectorAll(".operator");
 operatorBtn.forEach(btn => {
     btn.addEventListener('click', (e)=> {
         display.innerHTML = "";
-        console.log(e.target.value);
         if(operand2 == "0" && e.target.value == "="){
-            display.innerHTML = operand1;
+            display.innerHTML = limitResult(operand1);
+            operand1 = "0";
+            operator ="";
             console.log("= pressed with out 2opr")
         }
         else if(operand2 != "0"){
-            operand1 = operate(operator,parseInt(operand1),parseInt(operand2));
+            operand1 = operate(operator,Number(operand1),Number(operand2));
             result = operand1;
-            display.innerHTML = operand1;   
+            display.innerHTML = limitResult(operand1);   
             if(e.target.value == "="){
                 operator ="";
                 operand1 ="0";
